a3983881d8c9597ef703ae15bbede819beeea4af
ravi04ez <ezekie.em@gmail.com>
2022-09-29 07:45:20 +0530

    Added Daily Wages

--- employeewage.py
+++ employeewage.py
@@ -5,21 +5,27 @@ import logging
 class Employee:
     """
     class Employee
-    method: Check Attedance of Employee
+    method: Calculate Daily Wage of Employee
     """
 
-    def attendance(self):
+    def daily_wage(self):
         """
-        Function to Check attedance of Employee
-        :return: employee present or absent
+        Function to Calculate Employee Daily Wage
+        :return: Employee Daily Wages
         """
         try:
             is_present = 1
+            emp_rate_per_hrs = 20
+            emp_hrs = 0
             emp_check = random.randrange(0, 2)
             if emp_check == is_present:
                 print("Employee is Present")
+                emp_hrs = 8
             else:
                 print("Employee is Absent")
+                emp_hrs = 0
+            emp_wage = emp_hrs*emp_rate_per_hrs
+            print("Employee Wage : ", emp_wage)
         except Exception as err:
             print(err)
             logging.excption(err)
@@ -27,4 +33,4 @@ class Employee:
 
 if __name__ == "__main__":
     obj_emp = Employee()
-    obj_emp.attendance()
+    obj_emp.daily_wage()
